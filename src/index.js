ff75739c34506d5b054945c395a63f2a4c3869df
lmjing <lmjing4905@gmail.com>
2021-11-25 22:43:43 +0900

    1.2 Actions

--- src/index.js
+++ src/index.js
@@ -4,9 +4,19 @@ const minusBtn = document.getElementById('minus')
 const plusBtn = document.getElementById('plus')
 const number = document.querySelector('span')
 
-const reducer = (count = 0) => {
+const reducer = (count = 0, action) => {
+  const { type } = action
+
+  if (type === 'PLUS') return count + 1
+  if (type === 'MINUS') return count - 1
   return count
 }
 const countStore = createStore(reducer)
+
+countStore.dispatch({type: 'PLUS'})
+countStore.dispatch({type: 'PLUS'})
+countStore.dispatch({type: 'PLUS'})
+countStore.dispatch({type: 'PLUS'})
+countStore.dispatch({type: 'MINUS'})
 
 console.log(countStore.getState())
