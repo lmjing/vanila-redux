3ef21c26299a405a019499f7a30f27c13fd95b52
lmjing <lmjing4905@gmail.com>
2021-11-29 23:56:11 +0900

    2.1 State Mutation

--- src/index.js
+++ src/index.js
@@ -10,18 +10,22 @@ const DELETE_TODO = "DELETE_TODO"
 const reducer = (todos = [], action) => {
   const { type } = action
 
-  if (type === ADD_TODO) return todos
+  if (type === ADD_TODO) {
+    return [...todos, {text: action.text, id: Date.now() }]
+  }
   if (type === DELETE_TODO) return todos
   return todos
 }
 
 const store = createStore(reducer)
 
+store.subscribe(() => console.log(store.getState()))
+
 const onSubmit = e => {
   e.preventDefault()
   const todo = input.value
-  console.log(todo)
   store.dispatch({type: ADD_TODO, text: todo})
+  input.value = ""
 }
 
 form.addEventListener("submit", onSubmit)
